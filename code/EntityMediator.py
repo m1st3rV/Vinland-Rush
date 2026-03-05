607bef63e8a6e572078148ddd5ed0aae57770266
Danilo Venancio <danilovenanciof@gmail.com>
2026-03-05 20:13:49 -0300

    enemyshot configurado

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -3,6 +3,7 @@ from code.Enemy import Enemy
 from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 from code.PlayerShot import PlayerShot
+from code.Player import Player
 
 
 class EntityMediator:
@@ -21,10 +22,32 @@ class EntityMediator:
         pass
 
     @staticmethod
+    def __verify_collision_entity(ent1, ent2):
+        valid_collision = False
+        if isinstance(ent1, Player) and isinstance(ent2, EnemyShot):
+            valid_collision = True
+        elif isinstance(ent1, EnemyShot) and isinstance(ent2, Player):
+            valid_collision = True
+        elif isinstance(ent1, PlayerShot) and isinstance(ent2, Enemy):
+            valid_collision = True
+        elif isinstance(ent1, Enemy) and isinstance(ent2, PlayerShot):
+            valid_collision = True
+        elif isinstance(ent1, Enemy) and isinstance(ent2, Player):
+            valid_collision = True
+        elif isinstance(ent1, Player) and isinstance(ent2, Enemy):
+            valid_collision = True
+
+        if valid_collision:
+            if
+
+    @staticmethod
     def verify_collision(entity_list: list[Entity]):
         for i in range(len(entity_list)):
-            test_entity = entity_list[i]
-            EntityMediator.__verify_collision_window(test_entity)
+            entity1 = entity_list[i]
+            EntityMediator.__verify_collision_window(entity1)
+            for j in range(i+1, len(entity_list)):
+                entity2 = entity_list[j]
+                EntityMediator.__verify_collision_entity(entity1, entity2)
 
 
     @staticmethod
